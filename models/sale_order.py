11365f6daf72093d58e2aadd1d5d8828cdbca67f
onlyone-odoo <matiasb@onlyone.com.ar>
2025-07-18 11:57:16 -0300

    fix sale_order

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,28 +1,26 @@
-from odoo import api, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.constrains(
-        "company_id", "partner_id", "partner_invoice_id", "partner_shipping_id"
-    )
-    def _check_company_id_out_model(self):
-        for rec in self:
-            if not rec.company_id:
-                continue
-            # Permitir si la company del partner es child de la del order
-            for field_name in [
-                "partner_id",
-                "partner_invoice_id",
-                "partner_shipping_id",
-            ]:
-                partner = rec[field_name]
-                if (
-                    partner
-                    and partner.company_id
-                    and partner.company_id.parent_id == rec.company_id
-                ):
-                    continue  # Bypass para childs
-                # Llama al super para el resto
-                super(SaleOrder, rec)._check_company_id_out_model()
+    def _check_company_domain(self, company, field=None):
+        self.ensure_one()
+        if field and field.name in [
+            "partner_id",
+            "partner_invoice_id",
+            "partner_shipping_id",
+        ]:
+            if not company:
+                return True
+            if company == self.company_id:
+                return True
+            # Permitir si la company del partner es child (descendiente) de la del order
+            if (
+                self.company_id
+                and company.parent_path
+                and f"{self.company_id.id}/" in company.parent_path
+            ):
+                return True
+            return False
+        return super()._check_company_domain(company, field=field)
