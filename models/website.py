abda3fa1c8a06bb2490336b3125c3788ecf8e60a
onlyone-odoo <matiasb@onlyone.com.ar>
2025-07-18 15:01:11 -0300

    sale_get_order

--- models/website.py
+++ models/website.py
@@ -9,7 +9,7 @@ class Website(models.Model):
 
     def sale_get_order(self, *args, **kwargs):
         user = self.env.user
-        if user.portal and user.partner_id.company_id:
+        if user.partner_id.company_id:
             self = self.with_company(user.partner_id.company_id)
             _logger.info(f"Switching to company: {self.company_id.name}")
         order = super().sale_get_order(*args, **kwargs)
