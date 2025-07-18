c8643ae5e774f8d612ed0ca07e3acdf6236721cc
onlyone-odoo <matiasb@onlyone.com.ar>
2025-07-18 12:53:52 -0300

    website 2

--- models/website.py
+++ models/website.py
@@ -1,33 +1,16 @@
 from odoo import models
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class Website(models.Model):
     _inherit = "website"
 
-    def sale_get_order(
-        self,
-        force_create=False,
-        code=None,
-        update_pricelist=False,
-        force_pricelist=False,
-    ):
-        # Llamamos super para obtener/crear el order standard (inicialmente en website.company_id)
-        order = super().sale_get_order(
-            force_create=force_create,
-            code=code,
-            update_pricelist=update_pricelist,
-            force_pricelist=force_pricelist,
-        )
-        # Si es portal user y partner tiene company distinta, switchamos
-        user = request.env.user
-        if (
-            order
-            and user.portal
-            and user.partner_id.company_id
-            and order.company_id != user.partner_id.company_id
-        ):
-            # Usamos sudo y contexto para bypass validaciones iniciales
-            order.sudo().with_context(
-                company_id=user.partner_id.company_id.id
-            ).company_id = user.partner_id.company_id
+    def sale_get_order(self, *args, **kwargs):
+        user = self.env.user
+        if user.portal and user.partner_id.company_id:
+            self = self.with_company(user.partner_id.company_id)
+            _logger.info(f"Switching to company: {self.company_id.name}")
+        order = super().sale_get_order(*args, **kwargs)
         return order
